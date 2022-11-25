82b3886f8b6ed8f838417d966154d16bd28bee60
George <gio.digmel@gmail.com>
2022-11-24 20:05:49 -0800

    chore: added icons

---
+++ icons/CheckIcon.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const CheckIcon = () => {
+  return (
+    <>
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-8 h-8 pr-2"
+      >
+        <path
+          d="M17 3H7C4.79086 3 3 4.79086 3 7V17C3 19.2091 4.79086 21 7 21H17C19.2091 21 21 19.2091 21 17V7C21 4.79086 19.2091 3 17 3Z"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </>
+  );
+};
---
+++ icons/MarkedIcon.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const MarkedIcon = () => {
+  return (
+    <>
+      <svg
+        width="24"
+        height="24"
+        viewBox="-2 -2 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-8 h-8 pr-2"
+      >
+        <path
+          d="M15 1H5C2.79086 1 1 2.79086 1 5V15C1 17.2091 2.79086 19 5 19H15C17.2091 19 19 17.2091 19 15V5C19 2.79086 17.2091 1 15 1Z"
+          fill="currentColor"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M7 10L9.25 12L13 8"
+          stroke="#18181B"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </>
+  );
+};
