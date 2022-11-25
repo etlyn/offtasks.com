5831fc12ac4f6b40df811568b1f064692fcd8460
George <gio.digmel@gmail.com>
2022-11-24 21:23:57 -0800

    chore: added reload on logo click

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,21 @@ import React, { useContext } from "react";
 import { Account } from "./Account";
 import { Logo, RocketIcon } from "../icons";
 import { AppContext } from "../context";
+import { useRouter } from "next/router";
 
 export const Header = () => {
   const { context } = useContext(AppContext);
+  const router = useRouter();
+
+  const handleReload = () => {
+    router.push("/");
+  };
 
   return (
     <div className="bg-zinc-800 pt-4 pb-2 px-6 md:px-24 flex justify-between  border-zinc-50 border-b border-opacity-10 ">
-      <Logo />
+      <button onClick={handleReload}>
+        <Logo />
+      </button>
       <div className="flex flex-row -mt-1 items-center text-zinc-50">
         <RocketIcon />
         <h1 className="mx-2">
